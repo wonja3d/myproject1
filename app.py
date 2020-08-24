52851ce1b60f9b5da26b4f84c79b2353467f643f
wonja3d <49478257+wonja3d@users.noreply.github.com>
2020-08-24 18:52:56 +0900

    수정
    * AWS 에 올리기
    * 스크랩이나 삭제시 YES/NO 문의
    * 코멘트 위치 고민
    * 스크랩 html 페이지 넘버 적용
    * +, - 위치 옆으로
    * 이미 스크랩된것은 + 없애기
    * 메인과 스크랩 페이지 html 이동

--- app.py
+++ app.py
@@ -11,30 +11,64 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/scrap')
 def scrap():
     return render_template('scrap.html')
 
 
-
 @app.route('/tvcf', methods=['GET'])
 def read_tvcf():
-    tvcf_k_list = list(db.tvcfk.find({},{'_id':False}))
-    tvcf_g_list = list(db.tvcfe.find({},{'_id':False}))
-    return jsonify({'result': 'success', 'tvcf_korea_infos': tvcf_k_list, 'tvcf_global_infos' : tvcf_g_list})
+    tvcf_k_list = list(db.tvcfk.find({}, {'_id': 0}))
+    tvcf_g_list = list(db.tvcfe.find({}, {'_id': 0}))
 
-@app.route('/scrap', methods=['GET'])
+    return jsonify({'result': 'success', 'tvcf_korea_infos': tvcf_k_list, 'tvcf_global_infos': tvcf_g_list})
+
+
+@app.route('/scrapread', methods=['GET'])
 def read_scrap():
-    scrap_list = list(db.scrap.find({},{'_id':False}))
-    return jsonify({'result': 'success', 'scrap_list': scrap_list})
+    scrap_list = list(db.scrap.find({}, {'_id': False}))
+    return jsonify({'result': 'success', 'scrap_list': scrap_list, 'msg' : "get ok"})
+
+@app.route('/delscrap', methods=['POST'])
+def del_scrap():
+    url = request.form.get('url')
+    if (db.scrap.find_one({"url":url})) is not None:
+        db.scrap.delete_one({"url":url})
+        msg = '삭제가 되었습니다.'
+    else:
+        db.scrap.insert_one(scrap_data)
+        msg = 'DB에 없음'
+
+
+
+    return jsonify({'result': 'success', 'msg' : msg})
+
 
 @app.route('/scrap', methods=['POST'])
 def post_scrap():
     url = request.form.get('url')
     comment = request.form.get('comment')
+    lang = request.form.get('lang')
 
-    return jsonify({'result': 'success', 'msg': url+comment})
+    if lang == 'kr':
+        scrap_find = db.tvcfk.find_one({"url": url})
+    else:
+        scrap_find = db.tvcfe.find_one({"url": url})
 
+    scrap_data = {'title': scrap_find['title'],
+                  'date': scrap_find['date'],
+                  'type': scrap_find['type'],
+                  'url': scrap_find['url'],
+                  'img_url': scrap_find['img_url'],
+                  'comment' : comment
+                  }
+    if (db.scrap.find_one({"url":url})) is not None:
+        msg = '이미 스크랩이 되어있습니다.'
+    else:
+        db.scrap.insert_one(scrap_data)
+        msg = '스크랩 완료'
+    return jsonify({'result': 'success', 'msg': msg})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
